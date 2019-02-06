be640556db08b2373c546656cdd1dff5781c7f35
Evan Martin <evan.martin@shaw.ca>
2019-02-05 18:18:46 -0800

    added note

--- src/component/Rules.js
+++ src/component/Rules.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 export default class Rules extends React.Component {
-
-    render(){
-        return (
-            <div>
-              <h2>🎯 𝙃𝙤𝙬 𝙩𝙤 𝙂𝙚𝙩 𝙋𝙤𝙞𝙣𝙩𝙨</h2>
-              <div align="left" style={{ marginLeft: "40px", marginRight: "40px" }}>
-                <ul>
-                  <p>𝟭𝟬𝟬𝗽𝘁𝘀: Like a post</p>
-                  <p>𝟮𝟬𝟬𝗽𝘁𝘀: Be one of the first 10 commenters on a post</p>
-                  <p>𝟯𝟬𝟬𝗽𝘁𝘀: Tag a friend (the friend needs to be following you)</p>
-                  <p>
-                    𝟱𝟬𝟬𝗽𝘁𝘀: Add Creator Code: 𝙁𝙊𝙍𝙁𝙊𝙍𝙏𝙎𝘼𝙆𝙀𝙎 (DM me a screenshot of my
-                    code as your Support A Creator code)
-                  </p>
-                </ul>
-              </div>
-            </div>
-          );
-    }
+  render() {
+    return (
+      <div>
+        <h2>🎯 𝙃𝙤𝙬 𝙩𝙤 𝙂𝙚𝙩 𝙋𝙤𝙞𝙣𝙩𝙨</h2>
+        <div align="left" style={{ marginLeft: "40px", marginRight: "40px" }}>
+          <ul>
+            <p>𝟭𝟬𝟬𝗽𝘁𝘀: Like a post</p>
+            <p>𝟮𝟬𝟬𝗽𝘁𝘀: Be one of the first 10 commenters on a post</p>
+            <p>𝟯𝟬𝟬𝗽𝘁𝘀: Tag a friend (the friend needs to be following you and)*</p>
+            <p>
+              𝟱𝟬𝟬𝗽𝘁𝘀: Add Creator Code: 𝙁𝙊𝙍𝙁𝙊𝙍𝙏𝙎𝘼𝙆𝙀𝙎 (DM me a screenshot of my
+              code as your Support A Creator code)
+            </p>
+            <p>* you're Instagram account cannot be private</p>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
